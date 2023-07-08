ab981fe411f5e9432ae39009c7e297cf5c6affa3
Igor Chornobtyvtsev <igor.c@siklu.com>
2023-07-08 18:12:54 +0300

    unable to establish ssh connection to ip

--- Ssh.py
+++ Ssh.py
@@ -1,105 +1,7 @@
 import time
 import sys
-
 import paramiko
-from colorama import Fore, Style
-from pysnmp import hlapi
-
-
-def construct_object_types(list_of_oids):
-    object_types = []
-    for oid in list_of_oids:
-        object_types.append(hlapi.ObjectType(hlapi.ObjectIdentity(oid)))
-    return object_types
-
-
-def construct_value_pairs(list_of_pairs):
-    pairs = []
-    for key, value in list_of_pairs.items():
-        pairs.append(hlapi.ObjectType(hlapi.ObjectIdentity(key), value))
-    return pairs
-
-
-def cast(value):
-    try:
-        return int(value)
-    except (ValueError, TypeError):
-        try:
-            return float(value)
-        except (ValueError, TypeError):
-            try:
-                return str(value)
-            except (ValueError, TypeError):
-                pass
-    return value
-
-
-def fetch(handler, count):
-    result = []
-    for i in range(count):
-        try:
-            error_indication, error_status, error_index, var_binds = next(handler)
-            if not error_indication and not error_status:
-                items = {}
-                for var_bind in var_binds:
-                    items[str(var_bind[0])] = cast(var_bind[1])
-                result.append(items)
-            else:
-                raise RuntimeError('Got SNMP error: {0}'.format(error_indication))
-        except StopIteration:
-            break
-    return result
-
-
-def get(target, oids, credentials, port=161, engine=hlapi.SnmpEngine(),
-        context=hlapi.ContextData()):
-    handler = hlapi.getCmd(
-        engine,
-        credentials,
-        hlapi.UdpTransportTarget((target, port)),
-        context,
-        *construct_object_types(oids)
-    )
-    return fetch(handler, 1)[0]
-
-
-def get_bulk(target, oids, credentials, count, start_from=0, port=161,
-             engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
-    handler = hlapi.bulkCmd(
-        engine,
-        credentials,
-        hlapi.UdpTransportTarget((target, port)),
-        context,
-        start_from, count,
-        *construct_object_types(oids)
-    )
-    return fetch(handler, count)
-
-
-def get_bulk_auto(target, oids, credentials, count_oid, start_from=0, port=161,
-                  engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
-    count = get(target, [count_oid], credentials, port, engine, context)[count_oid]
-    return get_bulk(target, oids, credentials, count, start_from, port, engine, context)
-
-
-def set(target, value_pairs, credentials, port=161, engine=hlapi.SnmpEngine(),
-        context=hlapi.ContextData()):
-    handler = hlapi.setCmd(
-        engine,
-        credentials,
-        hlapi.UdpTransportTarget((target, port)),
-        context,
-        *construct_value_pairs(value_pairs)
-    )
-    return fetch(handler, 1)[0]
-
-
-#####################################################################
-def check_set_done(response_command):
-    if "Set done" in response_command:
-        return True
-    else:
-        return False
+import datetime
 
 
 ####################################################################
@@ -120,7 +22,7 @@ class Ssh:
                                 username=self.user,
                                 password=self.password,
                                 look_for_keys=False,
-                                allow_agent=False)
+                                allow_agent=False, banner_timeout=200)
 
         except:
             print(f"unable to establish ssh connection to ip {server_ip}")
@@ -128,75 +30,30 @@ class Ssh:
 
         self.connection = self.client.invoke_shell()
 
-    def reconnect(self):
-        try:
-            self.client = paramiko.SSHClient()
-            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.client.connect(self.server_ip,
-                                port=self.server_port,
-                                username=self.user,
-                                password=self.password,
-                                look_for_keys=False,
-                                allow_agent=False)
-        except:
-            print('unable to establish ssh connection')
-
     def send_command(self, command):
         self.connection.send(command + '\n')
         time.sleep(2)
         output = self.connection.recv(4096)
         return output.decode()
 
-    def check_frequency_in_units(self):
-        command = 'show rf channel-width'
-        results = self.send_command(command)
-        split_string = results.split(" ")
-        my_num1 = split_string[-1]
-        channel_width = []
-        for i in my_num1[:5]:
-            if i.isdigit():
-                channel_width.append(i)
-            else:
-                pass
-        num = ''.join(channel_width)
-        command2 = "show rf tx-frequency"
-        results = self.send_command(command2)
-        split_string = results.split(" ")
-        my_num2 = split_string[-1]
-        the_frequency_tx = []
-        for i in my_num2[:5]:
-            if i.isdigit():
-                the_frequency_tx.append(i)
-            else:
-                pass
-        num2 = ''.join(the_frequency_tx)
-        command3 = "show rf rx-frequency"
-        results = self.send_command(command3)
-        split_string = results.split(" ")
-        my_num3 = split_string[-1]
-        the_frequency_rx = []
-        for i in my_num3[:5]:
-            if i.isdigit():
-                the_frequency_rx.append(i)
-            else:
-                pass
-        num3 = ''.join(the_frequency_rx)
-        print(num, num2, num3)
-
-    def get_shell(self):
-        return self.connection
-
     def close(self):
         if self.client.get_transport().is_active():
             self.client.close()
 
-    ##############################################################################
+####################################################################
+def send_ssh_command(ip, cmd):
+    print(f"[{datetime.datetime.now()}] -- send SSH command '{cmd}' to {ip}")
+    try:
+        ssh_connection = Ssh(ip, 22, "admin", "admin")
+        results = ssh_connection.send_command(cmd)
+        #print(f">>>>>{results}<<<<<")
+        print(f"{results}")
+        ssh_connection.close()
+    except:
+        print(f"SSH command '{cmd}' to {ip} failed")
+        sys.exit(2)
 
-    def copy_running_startup(self):
-        self.send_command("copy running-configuration startup-configuration")
+def sleep(new_sleep):
+    print(f"[{datetime.datetime.now()}] -- sleep {new_sleep:3} sec")
+    time.sleep(new_sleep)
 
-    def set_eth2(self, eth_type, value, value_prefix):
-        # print(f"Device details : {self.server_ip} -> set eth eth2 eth-type "
-        #       f"{eth_type} {value}{value_prefix}")
-        results = self.send_command(f"set eth eth2 eth-type {eth_type} {value}{value_prefix}")
-        print(f">>>>>{results}<<<<<")
--- main.py
+++ main.py
@@ -2,110 +2,65 @@ import time
 import sys
 import datetime
 
-from Ssh import Ssh
-from colorama import Fore, Style
+from Ssh import send_ssh_command
+from Ssh import sleep
 
 
-def check_connectivity_after(ip):
-    print("check connectivity after\n")
-    try:
-        ip_connection = Ssh(ip, 22, "admin", "admin")
-        ip_connection.close()
-    except:
-        #print(Fore.RED + "Error, no ssh to {ip} after sending command\nMight be a reboot please check logs" + Style.RESET_ALL)
-        print("Error, no ssh\n")
-    time.sleep(4 * 60)  # 4 minutes wait after checking if device has connection to ssh
-    time.sleep(10)
-
-
-def set_eth_eth2_eth_type(ip, eth_type, value_prefix, values_arr):
-    for val in values_arr:
-        try:
-            ip_connection = Ssh(ip, 22, "admin", "admin")
-            ip_connection.set_eth2(eth_type, val, value_prefix)
-            #ip_connection.copy_running_startup()
-            ip_connection.close()
-            print("ip_connection -- closed")
-            time.sleep(10)  # 10 seconds wait after sending command to device
-        except:
-            #print(Fore.RED + f"Error, no ssh to {ip} before sending command" + Style.RESET_ALL)
-            print(f"Error, no ssh to {ip} before sending command")
-
-
-def send_ssh_command(ip, cmd):
-    print(f"send SSH command '{cmd}' to {ip}")
-    try:
-        ssh_connection = Ssh(ip, 22, "admin", "admin")
-        results = ssh_connection.send_command(cmd)
-        print(f">>>>>{results}<<<<<")
-        ssh_connection.close()
-    except:
-        print(f"SSH command '{cmd}' to {ip} failed")
-        sys.exit(2)
-
-def calculate_sleep(iter_num):
-    iter_period = 20
-    sleep_min = 30
-    sleep_step = 30
-
-    iter_cur = iter_num % iter_period # 1,2,3,4,    0,1,2,3,4, 
+def calculate_sleep(sleep_min, sleep_max, sleep_step, iter_num):
+    iter_period = (sleep_max - sleep_min) // sleep_step
+    iter_cur = iter_num % iter_period
     new_sleep = sleep_min + iter_cur * sleep_step
-
-    print(f"iter:{iter_num} sleep:{new_sleep}")
+    return new_sleep
 
 
 if __name__ == '__main__':
 
-    iteration = 1
-    max_iteration = 100
-    for iteration in range(1, max_iteration + 1):
-    #while iteration <= max_iteration:
-        calculate_sleep(iteration)
+    #iteration = 1
 
+    sleep_min = 30
+    sleep_max = 600
+    sleep_step = 10
+    iter_period = (sleep_max - sleep_min) // sleep_step
+
+    max_iteration = (sleep_max - sleep_min) // sleep_step
+
+
+    total_time = 0
+
+    devA_ip = "10.61.117.33"
+
+    for iteration in range(1, max_iteration):
+        print(f"[{datetime.datetime.now()}] -- Start iteration number {iteration}")
+
+        send_ssh_command(devA_ip, "show eth eth2")
+
+        new_sleep = calculate_sleep(sleep_min, sleep_max, sleep_step, iteration)
+        sleep(new_sleep)
+
+
+        # total_time += new_sleep
+        # total_min = total_time // 60
+        # print(f"iter:{iteration:4} sleep:{new_sleep:5} total_time:{total_time:5} total_min:{total_min}")
+
+        #send_ssh_command(devA_ip, "show eth eth2")
+        #sleep(new_sleep)
+
+    print("DONE")
     sys.exit(0)
 
 
-    devA_ip = "10.61.117.33"
-    max_iteration = 100
-    sleep_after_command = 3 * 60
+    # max_iteration = 100
 
-    iteration = 1 
-    while iteration <= max_iteration:
-        now = datetime.datetime.now()
-        print(f"-- Start iteration number {iteration} -- {now}")
-        send_ssh_command(devA_ip, "set eth eth2 eth-type rj45 1000fd")
-        time.sleep(sleep_after_command)
+    # iteration = 1 
+    # while iteration <= max_iteration:
+    #     now = datetime.datetime.now()
+    #     print(f"-- Start iteration number {iteration} -- {now}")
+    #     #send_ssh_command(devA_ip, "set eth eth2 eth-type rj45 1000fd")
+    #     send_ssh_command(devA_ip, "show eth eth2")
+    #     time.sleep(sleep_after_command)
 
-        send_ssh_command(devA_ip, "set eth eth2 eth-type rj45 10000fd")
-        time.sleep(sleep_after_command)
+    #     # send_ssh_command(devA_ip, "set eth eth2 eth-type rj45 10000fd")
+    #     # time.sleep(sleep_after_command)
 
-        iteration += 1
-    print("DONE")
-
-
-
-# #########################
-# EH-8010FX-L>show ip
-
-# ip 1 ip-addr                   : dhcp 10.61.118.6
-# ip 1 prefix-len                : 24
-# ip 1 vlan                      : 0
-# ip 1 default-gateway           : 10.61.118.1
-
-# ip 2 ip-addr                   : static 10.61.117.33
-# ip 2 prefix-len                : 24
-# ip 2 vlan                      : 0
-# ip 2 default-gateway           : 0.0.0.0
-
-
-# EH-8010FX-H>show ip
-
-# ip 1 ip-addr                   : dhcp 10.61.118.5
-# ip 1 prefix-len                : 24
-# ip 1 vlan                      : 0
-# ip 1 default-gateway           : 10.61.118.1
-
-# ip 2 ip-addr                   : static 10.61.118.33
-# ip 2 prefix-len                : 24
-# ip 2 vlan                      : 0
-# ip 2 default-gateway           : 10.61.118.1
+    #     iteration += 1
+    # print("DONE")
